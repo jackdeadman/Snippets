064e28088859c2db02bd18d9d589b47edf4af201
nocturnaltortoise <simonturner34@gmail.com>
2014-04-27 17:55:36 +0100

    simplifying imports, neatening up code, altering comments

--- src/graphics/ImageLoader.java
+++ src/graphics/ImageLoader.java
@@ -1,22 +1,18 @@
 package graphics;
 
-import java.awt.Image;
-import java.io.IOException;
 import java.net.URL;
 
 public class ImageLoader {
 	//This is a small method I made to simplify loading of images.
 		
-	private URL loadTexture(String filename){
-		URL tempImg;
-		        
-		tempImg = this.getClass().getResource(filename);
+	private URL loadTexture(String filename){   
+		URL tempImg = this.getClass().getResource(filename);
 		        
 		return tempImg;
-		
 	}
 
-//Just use this line of code!
+//To load an image with this function, just use:
 
 //image = loadTexture(loadResource("whatever.png"));
+// with image being an Image, or a ImageIcon, or anything that would accept a URL.
 }
